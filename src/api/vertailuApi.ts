5bf723fd7fc985cf67927d215fcdd4ead8905e14
Alina Snäkin <alina.snakin@oppi.luovi.fi>
2026-02-11 08:41:10 +0200

    Poistettu käyttämättömät tiedostot

--- src/api/vertailuApi.ts
+++
@@ -1,5 +0,0 @@
-import { luoTulos } from "../logic/vertailut";
-
-export function lahetaVertailu(voittajaId: string, haviajaId: string) {
-  return luoTulos(voittajaId, haviajaId);
-}
--- src/db/rakenne.ts
+++
@@ -1,7 +0,0 @@
-export type KoostePisteet = {
-  ammattiId: string;
-  voitot: number;
-  haviot: number;
-};
-
-export const pistetaulu: KoostePisteet[] = [];
