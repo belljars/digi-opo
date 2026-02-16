d25e7ed97948c93644c12011d6feb0aa50695af0
Alina Snäkin <alina.snakin@oppi.luovi.fi>
2026-02-16 10:58:45 +0200

    Lisätty puuttuvat skriptikopiot ja virheiden käsittely layout.js- ja opintopolut.js-tiedostoihin

--- run_linux.sh
+++ run_linux.sh
@@ -12,4 +12,6 @@ pip install -r requirements.txt
 npm run build
 cp dist/ui/scripts/main.js src/ui/scripts/main.js
 cp dist/ui/scripts/quiz.js src/ui/scripts/quiz.js
+cp dist/ui/scripts/layout.js src/ui/scripts/layout.js
+cp dist/ui/scripts/opintopolut.js src/ui/scripts/opintopolut.js
 python3 src/desktop/app.py
